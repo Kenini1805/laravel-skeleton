29443edf2e9672d2fcafd805118a736e62956fd5
TungPro <mr.nttung@gmail.com>
2017-07-18 11:32:20 +0700

    Laravel skeleton installer

---
+++ src/RunCommand.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace Framgia\Laravel\Skeleton\Console;
+
+use ZipArchive;
+use RuntimeException;
+use GuzzleHttp\Client;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
+
+class RunCommand extends Command
+{
+    /**
+     * Configure the command options.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('run')
+            ->setDescription('Create a new Laravel skeleton application.')
+            ->addArgument('name', InputArgument::OPTIONAL);
+    }
+
+    /**
+     * Execute the command.
+     *
+     * @param  \Symfony\Component\Console\Input\InputInterface  $input
+     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+     * @return void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        if (! class_exists('ZipArchive')) {
+            throw new RuntimeException('The Zip PHP extension is not installed. Please install it and try again.');
+        }
+
+        $directory = ($input->getArgument('name')) ? getcwd().'/'.$input->getArgument('name') : getcwd();
+
+        $output->writeln('<info>Creating Laravel skeleton...</info>');
+
+        $this->download($zipFile = $this->makeFilename())
+            ->extract($zipFile, $directory)
+            ->removeUserModel($directory)
+            ->cleanUp($zipFile);
+
+        $output->writeln('<comment>Done! Do something!</comment>');
+    }
+
+    /**
+     * Delete old model User.php
+     *
+     * @return $this
+     */
+    protected function removeUserModel($directory)
+    {
+        $modelFile = $directory . 'app/User.php';
+
+        if (is_file($modelFile)) {
+            @chmod($modelFile, 0777);
+            @unlink($modelFile);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Generate a random temporary filename.
+     *
+     * @return string
+     */
+    protected function makeFilename()
+    {
+        return getcwd().'/laravel_skeleton_'.md5(time().uniqid()).'.zip';
+    }
+
+    /**
+     * Download the temporary Zip to the given file.
+     *
+     * @param  string  $zipFile
+     * @param  string  $version
+     * @return $this
+     */
+    protected function download($zipFile)
+    {
+        $response = (new Client)->get('https://raw.githubusercontent.com/framgia/laravel-skeleton/master/skeleton.zip');
+        file_put_contents($zipFile, $response->getBody());
+
+        return $this;
+    }
+
+    /**
+     * Extract the Zip file into the given directory.
+     *
+     * @param  string  $zipFile
+     * @param  string  $directory
+     * @return $this
+     */
+    protected function extract($zipFile, $directory)
+    {
+        $archive = new ZipArchive;
+        $archive->open($zipFile);
+        $archive->extractTo($directory);
+        $archive->close();
+
+        return $this;
+    }
+
+    /**
+     * Clean-up the Zip file.
+     *
+     * @param  string  $zipFile
+     * @return $this
+     */
+    protected function cleanUp($zipFile)
+    {
+        @chmod($zipFile, 0777);
+        @unlink($zipFile);
+
+        return $this;
+    }
+}
