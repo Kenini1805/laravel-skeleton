de72520ac18f006968b1129f534114edf01e7cbc
TungPro <mr.nttung@gmail.com>
2017-07-19 14:16:34 +0700

    update zipper.sh

--- zipper.sh
+++ zipper.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd laravel-base
-git archive --format=zip HEAD `git diff HEAD^ HEAD --name-only` > ../skeleton.zip
+git archive --format=zip HEAD `git diff $(git rev-list --max-parents=0 HEAD) HEAD --name-only` > ../skeleton.zip
 cd ../..
 
